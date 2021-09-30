25cd7fab052304aea5801214221df6a4d4645a5b
BENK Asma <77125092+Asma-6@users.noreply.github.com>
2021-09-30 06:05:28 +0100

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,200 +1,197 @@
 # ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
+# Module: lexer.py
+# IT's about a tokenizer
 # ------------------------------------------------------------
-import ply.lex as lex  # inside lex there is import re for regex
+
+import ply.lex as lex
+
+############################ RULES ###########################
 
 # List of token names.   This is always required
+# tuple
 tokens = [
-   'ID'     , 'INT'    , 'FLOAT'   , 'CHAR'   , 'STR'    ,
-    # Operators
-   'PLUS'   , 'MINUS'  , 'MUL'     , 'DIV'   , 'FLOORDIV', 'MOD'     , 'POW',
-   'ASSIGN' , 'PLUSASS', 'MINUSASS', 'MULASS', 'DIVASS'  , 'MODASS'  ,
-   'GTHAN'  , 'LTHAN'  , 'EQUAL'   , 'NOTEQ' , 'GTORE'   , 'LTORE'   ,
-   # Symbols
-   'LPAREN' , 'RPAREN' , 'LBRACE'  , 'RBRACE' , 'LBRACKET', 'RBRACKET',
-   'SLICE'  , 'COMMA'  , 'PERIOD'  , 'QUOTE'  , 'SINGLQ'  , 'CONDOP'  ,
-   'DECORAT', 'CARET'  , 'TILDE'       
+    # Identifiers
+    'ISM'                  ,
+    # Numbers
+    'RA9MS7I7'             ,
+    'RA9M3ACHARI'          ,
+    # Characters and Strings
+    '7ARF'                 ,
+    'JOMLA'                ,
+    # Arithmetic operators
+    'ZA2ID'                ,
+    'NA9IS'                ,
+    'FI'                   ,
+    'M9SOUM3LA'            ,
+    '9ISMAS7I7A'           ,
+    'LBA9IDYALO3LA'        ,
+    'OUS'                  ,
+    # Comparative operators
+    'KBARMN'               ,
+    'SGHARMN'              ,
+    'KAYSAWI'              ,
+    'KAYKHALF'             ,
+    'KBARMNWLAKAYSAWI'     ,
+    'SGHARMNWLAKAYSAWI'    ,
+    # Assignment operators
+    'KIYAKHOD'             ,
+    'ZID3LIH'              ,
+    'N9ESSMNO'             ,
+    'DRBOFI'               ,
+    '9SMO3LA'              ,
+    'BA9IH3LA'             ,
+    # Symbols
+    '9AWSLISER'            ,
+    '9AWSLIMEN'            ,
+    'LAMMALISRIYA'         ,
+    'LAMMALIMNIYA'         ,
+    'MA39OUFALISRIYA'      ,
+    'MA39OUFALIMNIYA'      ,
+    'JOJNO9AT'             ,
+    'FASILA'               ,
+    # Newline
+    'STERJDID'             ,
+    'TAB'                  ,
 ]
 
+# Dictionary for reserved words
 reserved = {
-   # Regex for logical operators
-   'w'         : 'W',
-   'wla'       : 'WLA',
-   'machi'     : 'MACHI',
-   # Regex for identity operators
-   'howa'      : 'HOWA',
-   'howamachi' : 'HOWAMACHI',
-   # Regex for membership operators
-   'kaynf'     : 'KAYNF',
-   'makaynchf' : 'MAKAYNCHF',
-   # Boolean values
-   's7i7'      : 'S7I7',
-   'khate2'    : 'KHATE2',
-   # Loops
-   '3la9bel'   : '3LA9BEL',
-   'ma7ed'     : 'MA7ED',
-   # Functions 
-   'kteb'      : 'KTEB',
-   '9ra'       : '9RA',
-   'rjja3'     : 'RJJA3',
-   'mjmo3a'    : 'MJMO3A',
-   'nou3'      : 'NOU3',
-   '7el'       : '7EL',
-   'sed'       : 'SED',
-   # Reserved words
-   'ilakan'    : 'ILAKAN',
-   'wilakan'   : 'WILAKAN',
-   'ilamakanch': 'ILAMAKANCH',
-   'men'       : 'MEN',
-   'jib'       : 'JIB',
-   '3lachkel'  : '3LACHKEL',
-   '3rref'     : '3RREF',
-   '7iyed'     : '7IYED',
-   'ta7aja'    : 'TA7AJA',
-   'khrej'     : 'KHREJ',
-   'kmel'      : 'KMEL',
-   'douz'      : 'DOUZ',
-   '3iyet3la'  : '3IYET3LA',
-   'jreb'      : 'JREB'
-   #class?
+    # Regex for membership operators
+    'kaynf'         : 'KAYNF'      ,
+    # Boolean values
+    's7i7'          : 'S7I7'       ,
+    'khate2'        : 'KHATE2'     ,
+    # Loops
+    'bnisbal'       : 'BNISBAL'    ,
+    'ma7ed'         : 'MA7ED'      ,
+    'ilamakanch'    : 'ILAMAKANCH' ,
+    # Functions
+    'kteb'          : 'KTEB'       ,
+    '_9ra'          : '_9RA'       ,
+    # Reserved words
+    'ilakan'        : 'ILAKAN'     ,
+    'ila'           : 'ILA'        ,
+    'wilamakanch'   : 'WILAMAKANCH',
+    'khrej'         : 'KHREJ'      ,
+    'kmel'          : 'KMEL'       ,
+    'nita9'         : 'NITA9'      ,
+    'wla'           : 'WLA'        ,
+    'w'             : 'W'
 }
+
 
 tokens = tokens + list(reserved.values())
 
-# Regular expression rules for simple tokens (defined by strings)
-t_CHAR       = r'.'
+# Regular expression rules for simple tokens
+# Regex for arithmetic operators
+t_ZA2ID              = r'\+'
+t_NA9IS              = r'-'
+t_FI                 = r'\*'
+t_M9SOUM3LA          = r'/'
+t_LBA9IDYALO3LA      = r'\%'
+t_9ISMAS7I7A         = r'/{2}'
+t_OUS                = r'\*{2}'
 
-  # Regex for arithmetic operators
-t_PLUS       = r'\+'   # or enclose it inside a character class as [+]
-t_MINUS      = r'-'
-t_MUL        = r'\*'
-t_DIV        = r'/'
-t_FLOORDIV   = r'/{2}'  # integer division
-t_MOD        = r'%'
-t_POW        = r'\*{2}'
+# Regex for assignment operators
+t_KIYAKHOD           = r'='
+t_ZID3LIH            = r'\+='
+t_N9ESSMNO           = r'-='
+t_DRBOFI             = r'\*='
+t_9SMO3LA            = r'/='
+t_BA9IH3LA           = r'\%='
 
-  # Regex for assignment operators
-t_ASSIGN     = r'\='
-t_PLUSASS    = r'\+='
-t_MINUSASS   = r'-='
-t_MULASS     = r'\*='
-t_DIVASS     = r'/='
-t_MODASS     = r'%='
-
-  # Regex for comparison operators
-t_GTHAN      = r'\>'
-t_LTHAN      = r'\<'
-t_EQUAL      = r'={2}'
-t_NOTEQ      = r'!\='
-t_GTORE      = r'>\=' 
-t_LTORE      = r'<\='
-
-  # Regex for symbols
-t_LPAREN     = r'\('
-t_RPAREN     = r'\)'
-t_LBRACE     = r'{'
-t_RBRACE     = r'}'
-t_LBRACKET   = r'\['
-t_RBRACKET   = r']'
-t_SLICE      = r'\:'
-t_COMMA      = r',' 
-t_PERIOD     = r'\.'
-t_QUOTE      = r'\"'
-t_SINGLQ     = r'\''
-t_CONDOP     = r'\?'
-t_DECORAT    = r'\@'
-t_CARET      = r'\^'
-t_TILDE      = r'\~'
+# Regex for comparative operators
+t_KBARMN             = r'\>'
+t_SGHARMN            = r'\<'
+t_KAYKHALF           = r'\!='
+t_KAYSAWI            = r'\=='
+t_KBARMNWLAKAYSAWI   = r'\>='
+t_SGHARMNWLAKAYSAWI  = r'\<='
 
 
-# A regular expression rule with some action code for tokens (defined by functions)
-  # the ordre of the regex is so important here
-def t_FLOAT(t):
-    r'\d+\.\d*'                  
-    t.value = float(t.value) 
+# Regex for symbols
+t_9AWSLISER          = r'\('
+t_9AWSLIMEN          = r'\)'
+t_MA39OUFALISRIYA    = r'\['
+t_MA39OUFALIMNIYA    = r'\]'
+t_LAMMALISRIYA        = r'\{'
+t_LAMMALIMNIYA        = r'\}'
+t_JOJNO9AT           = r':'
+t_FASILA             = r','
+
+literals = "?@."
+
+
+# A regular expression rule with some action code
+
+# Regex for identifiers and numbers
+def t_ISM(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ISM')  # Check for reserved words
     return t
 
-def t_INT(t):
-    r'\d+'                  
+def t_RA9M3ACHARI(t):
+    r'[+-]?[0-9]+\.[0-9]*'
+    t.value = float(t.value)
+    return t
+
+def t_RA9MS7I7(t):
+    r'[+-]?[0-9]+'
     t.value = int(t.value)
     return t
 
+# Ignore comments (comment's rule should be before 7ARF and JOMLA, otherwise it'll not be considered)
+def t_T3LI9(t):
+    r'\#.*|\'{3}.*\'{3}'
+    pass
 
- 
-'''def t_NUMBER(t):
-    r'\d+'                   # is equivalent to [0-9]+
-    t.value = float(t.value) if '.' in t.value else int(t.value)  # converts the string into a Python float or integer
-    return t  ''' 
-
-def t_ID(t):
-    r'[a-zA-Z_]\w*'          # \w* is equivalent to [a-zA-Z0-9_]*
-    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+# Regex for characters and strings
+def t_7ARF(t):
+    r'\'[^\']?\''
     return t
 
-def t_COMMENT(t):
-    r'\#.*'
-    pass
-    # No return value. Token discarded, it's not necessary to define it in tokens
+def t_JOMLA(t):
+    r'\'[^\']*\'|\"[^\"]*\"'
+    return t
 
 # Define a rule so we can track line numbers
-def t_newline(t):
+def t_STERJDID(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-    
-# EOF handling rule
-def __init__(self):
-    def t_eof(t):
-        # Get more input (Example)
-        more = input('... ')
-        if more:
-            self.lexer.input(more)
-            return self.lexer.token()
-        return None
+    return t
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+# Rule for tabs
+def t_TAB(t):
+    r'([ \f]{4})+'
+    return t
+
+# Ignore spaces
+def t_FARAGH(t):
+    r'[ \f]+'
+    pass
+
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Had 7arf khate2 '%s'" % t.value[0])
     t.lexer.skip(1)
 
 
+
+############################ MAIN ###########################
 # Build the lexer
 lexer = lex.lex()
 
-# Test it out
-data = '''
-2. + 5 = 12
-
-ma7ed a < b:
-    kteb('6')
-'''
 
 # Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-'''while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    print(tok)
-
-for tok in lexer:
-    print(tok)'''
+codeSource = open('codeSource.py', 'r')
+lexer.input(codeSource.read())
 
 # Tokenize
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
-    print(tok.type, tok.value, tok.lineno, tok.lexpos)
+        break  # No more input
+    #print(tok.type, tok.value, tok.lineno, tok.lexpos)   # Showing the tokens
 
-
-
-
-
+codeSource.close()
