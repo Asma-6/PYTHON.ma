807fdace740503c9c8675f8acc8a539f0951a5e7
BENK Asma <77125092+Asma-6@users.noreply.github.com>
2021-10-23 20:30:03 +0100

    Update codeSource.py

--- codeSource.py
+++ codeSource.py
@@ -3,14 +3,14 @@ b = -6.5
 kteb(a + b)
 kteb(a>b)
 
-c = _9ra(" dkhel smitek: ")
+c = _9ra(" dkhel smiya: ")
 ma7ed c != "ahl" :
     {kteb('smiya'" khat2a", c)
-    c = _9ra(" dkhel smitek: ")}
+    c = _9ra(" dkhel smiya: ")}
 ilamakanch:
     {kteb('smiya'+' s7i7a')
      khrej}
 
-ila b < 0 : kteb(b , "Ra9m negatif!")
+ila b < 0 : kteb(b , "Ra9m salib!")
 bnisbal a kaynf nita9(3):
     {kteb(a)}
